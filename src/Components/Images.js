e58887e321825e64999d00b991df407e11fa8cf7
Abhishek Garain <abhi211199@gmail.com>
2021-09-21 18:23:26 +0530

    added loader

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -2,22 +2,24 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Card from './Card';
-import {AppProvider} from "@shopify/polaris";
+import {AppProvider, Spinner} from "@shopify/polaris";
 import en from '@shopify/polaris/locales/en.json';
 
 export default function Images(props) {
     const [data, setData] = useState([]);
+    const [loader, setLoader] = useState(false);
 
     useEffect(()=>{
         let startDate=window.localStorage.getItem("startDate"), endDate=window.localStorage.getItem("endDate");
         let URL = `${process.env.REACT_APP_URL}`;
         if(startDate) URL+=`&start_date=${startDate}`;
         if(endDate) URL+=`&end_date=${endDate}`;
-        console.log(URL)
+        setLoader(true);
         axios.get(URL)
         .then(function (response) {
             setData(response.data);
             props.count(response.data.length);
+            setLoader(false);
         })
         .catch(function (error) {
             console.log(error);
@@ -26,8 +28,9 @@ export default function Images(props) {
 
     return(
         <AppProvider i18n={en} theme={{colorScheme: "light"}}>
+            {loader && <Spinner accessibilityLabel="Spinner example" size="large" />}
             {
-                data.map((val)=> {
+                !loader && data.map((val)=> {
                     if(val?.media_type==="image")
                     return(<Card {...val} trigger={()=>props.trigger()} />)
                 })
